477d6dfb1b9d52a8dda7d7bd562d53a9a69ab83d
Craig McKeachie <craigdmckeachie@gmail.com>
2019-01-02 23:25:00 -0500

    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { Photo } from './photo.model';
 import { PhotoService } from './photo.service';
+import { retry, retryWhen, delay, take, concat } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   template: `
     <h1>Photos</h1>
     <div *ngFor="let photo of photos">
-      <img [src]="photo.thumbnailUrl">
-      <p>{{photo.title}}</p>
+      <img [src]="photo.thumbnailUrl" />
+      <p>{{ photo.title }}</p>
     </div>
   `,
   styles: []
@@ -17,6 +19,22 @@ export class AppComponent implements OnInit {
   photos: Photo[];
   constructor(private photoService: PhotoService) {}
   ngOnInit(): void {
-    this.photoService.getAll().subscribe(data => (this.photos = data));
+    this.photoService
+      .getAll()
+      .pipe(
+        // retry(3)
+        retryWhen(error => {
+          return error.pipe(
+            delay(1000),
+            take(5),
+            concat(
+              throwError({
+                error: 'Sorry, there was an error (after 5 retries)'
+              })
+            )
+          );
+        })
+      )
+      .subscribe(data => (this.photos = data));
   }
 }
